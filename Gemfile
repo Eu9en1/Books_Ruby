854dbca6d820474ed5fee328d9b8ea6f13a3786f
Evgenii <burakin.evgenij@yandex.ru>
2022-04-09 20:49:50 +0300

    new menu

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ source 'https://rubygems.org'
 
 # gem "rails"
 
-gem 'rubocop', '~> 1.22'
+gem 'json'
+gem 'rubocop'
--- lib/menu.rb
+++ lib/menu.rb
@@ -16,12 +16,30 @@ class Menu
     json = JSONParser.read_json
     books = JSONParser.read_books(json)
     publishing_houses = JSONParser.read_publishing_houses(json)
+    
     print_info(books, publishing_houses)
+    
     loop do
       choise = menu_loop
-      if choise == 2
+      if choise == 3
         print("Работа программы завершена пользователем\n")
         break
+      end
+      if choise == 2
+        puts(books.random_book(json))
+        loop do
+          choise = menu_loop_sec
+          if choise == 3
+            print("Работа программы завершена пользователем\n")
+            abort
+          end
+          if choise == 2
+            puts(books.random_book(json))
+          end
+          if choise == 1
+            break
+          end
+        end
       end
       puts(books.random_book(json)) if choise == 1
     end
@@ -32,16 +50,18 @@ class Menu
     puts('Количество книг, доступных для предзаказа:'\
        "#{books.preorder_allow_count}")
     puts('Два издательства, чьих книг в магазине'\
-       "на данный момент больше всего: #{publishing_houses.most_books(books)}")
+       " на данный момент больше всего: #{publishing_houses.most_books(books)}")
   end
 
   def self.menu_loop
     print("1. Случайная книга\n")
-    print("2. Выход\n")
-    print('Введите номер нужнного пункта меню: ')
+    print("2. Случайный автор\n")
+    print("3. Выход\n")
+    print('Введите номер нужного пункта меню: ')
     choise = gets.to_i
+    
     loop do
-      break if [2, 1].include?(choise)
+      break if [3, 2, 1].include?(choise)
 
       print('Номер пункта меню введен неверно, введите еще раз: ')
       choise = gets.to_i
@@ -49,4 +69,22 @@ class Menu
 
     choise
   end
+
+  def self.menu_loop_sec
+    print("1. Назад к первому меню\n")
+    print("2. Другой случайный автор\n")
+    print("3. Выход\n")
+    print('Введите номер нужнного пункта меню: ')
+    choise = gets.to_i
+    
+    loop do
+      break if [3, 2, 1].include?(choise)
+
+      print('Номер пункта меню введен неверно, введите еще раз: ')
+      choise = gets.to_i
+    end
+
+    choise
+  end
+
 end
